cd3a031d48d62cc8cc8b21787c4ba8c534d980d0
jinho-waah <137701918+jinho-waah@users.noreply.github.com>
2025-01-31 22:55:56 +0900

    [Gold IV] Title: 알파벳, Time: 1204 ms, Memory: 113308 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,26 @@
+
+dp = {}
+
+def dfs(x, y, mask):
+    if (x, y, mask) in dp:
+        return dp[(x, y, mask)]
+    
+    dp[(x, y, mask)] = 1
+    for dx, dy in [(0,1),(1,0),(0,-1),(-1,0)]:
+        nx, ny = x+dx, y+dy
+        if 0 <= nx < r and 0 <= ny < c:
+            alpha_idx = ord(board[nx][ny]) - ord('A')
+            nxt_bit = (1 << alpha_idx)
+            if not (mask & nxt_bit):
+                dp[(x, y, mask)] = max(dp[(x, y, mask)],
+                                       1 + dfs(nx, ny, mask | nxt_bit))
+    return dp[(x, y, mask)]
+
+r, c = map(int, input().split())
+board = [input().rstrip() for _ in range(r)]
+
+start_char = board[0][0]
+start_mask = 1 << (ord(start_char) - ord('A'))
+
+ans = dfs(0, 0, start_mask)
+print(ans)
